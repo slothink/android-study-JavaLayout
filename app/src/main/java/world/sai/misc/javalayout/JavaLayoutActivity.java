de43958e691f122cc4ed633d9a859fa4f665aead
sai.san <sai.san@daumkakao.com>
2015-08-27 15:28:00 +0900

    p202 까지 완료

--- app/src/main/java/world/sai/misc/javalayout/JavaLayoutActivity.java
+++ app/src/main/java/world/sai/misc/javalayout/JavaLayoutActivity.java
@@ -1,16 +1,55 @@
 package world.sai.misc.javalayout;
 
 import android.app.Activity;
+import android.content.res.Resources;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RelativeLayout;
 
 public class JavaLayoutActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_java_layout);
+        //setContentView(R.layout.activity_java_layout);
+
+        Button button = new Button(this);
+        button.setText("Press Me");
+        button.setBackgroundColor(Color.YELLOW);
+        button.setTransformationMethod(null);
+        button.setAllCaps(false);
+
+        RelativeLayout.LayoutParams buttonParams = new RelativeLayout.LayoutParams(
+                RelativeLayout.LayoutParams.WRAP_CONTENT,  RelativeLayout.LayoutParams.WRAP_CONTENT);
+//        RelativeLayout.LayoutParams.MATCH_PARENT,  RelativeLayout.LayoutParams.MATCH_PARENT);
+        buttonParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        buttonParams.addRule(RelativeLayout.CENTER_VERTICAL);
+
+        RelativeLayout myLayout = new RelativeLayout(this);
+        myLayout.addView(button, buttonParams);
+        myLayout.setBackgroundColor(Color.BLUE);
+
+        EditText myEditText = new EditText(this);
+        button.setId(1);
+        myEditText.setId(2);
+
+        RelativeLayout.LayoutParams textParams = new RelativeLayout.LayoutParams(RelativeLayout.LayoutParams.WRAP_CONTENT,  RelativeLayout.LayoutParams.WRAP_CONTENT);
+        textParams.addRule(RelativeLayout.ABOVE, button.getId());
+        textParams.addRule(RelativeLayout.CENTER_HORIZONTAL);
+        textParams.setMargins(0, 0, 0, 80);
+
+        Resources r = getResources();
+        int px = (int) TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, 320, r.getDisplayMetrics());
+        myEditText.setWidth(px);
+
+
+        myLayout.addView(myEditText, textParams);
+        setContentView(myLayout);
     }
 
     @Override
